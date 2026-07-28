e2652b6dc22482089cd73e50f6b4e27272f0e996
codedbyshreena <shreenasolia3097@gmail.com>
2026-07-28 22:29:08 +0530

    lab 09 added

--- lab09/reverselist.c
+++ lab09/reverselist.c
@@ -6,11 +6,56 @@ struct node {
     struct node *link;
 };
 
-stuct node *first = NULL;
+struct node *first = NULL;
 
-void insertAtFirst() {
-    struct node *newNode;
+void insertAtFirst(int x) {
+    struct node *newNode=(struct node*)malloc(sizeof(struct node));
 
     newNode->info = x;
+    newNode->link = first;
+
+    first = newNode;
+}
+
+void display() {
+    struct node *save = first;
+
+    if(save == NULL) {
+        printf("The list is empty.");
+    }
+
+    while(save != NULL) {
+        printf("%d -> ",save->info);
+        save = save->link;
+    }
+}
+
+void reverse() {
+    struct node *prev = NULL;
+    struct node *save = first;
+    struct node *next = NULL;
+
+    while(save != NULL) {
+        next = save->link;
+        save->link = prev;
+
+        prev = save;
+        save = next;
+    }
+
+    first = prev;
+}
+
+void main() {
+    insertAtFirst(9);
+    insertAtFirst(8);
+    insertAtFirst(7);
+    insertAtFirst(6);
+
+    display();
+    printf("Reversed List\n");
     
+    reverse();
+
+    display();
 }
